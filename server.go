bea97db8fadc8d562e27e9ebaea04ab6d91f9def
Agam Agarwal <agammaster@gmail.com>
2021-04-27 13:51:32 +0530

    Add flag for directory to serve

--- server.go
+++ server.go
@@ -10,22 +10,25 @@ import (
 
 var timeFormat = "Jan 2 2006 15:04:05"
 
-type middleware struct{}
+type middleware struct{
+	dir string
+}
 
-func (*middleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (m *middleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("%s %s from %s at %s\n", r.Method, r.URL, r.RemoteAddr,
 		time.Now().Format(timeFormat))
-	http.FileServer(http.Dir(".")).ServeHTTP(w, r)
+	http.FileServer(http.Dir(m.dir)).ServeHTTP(w, r)
 }
 
 func main() {
 	portPtr := flag.Int("port", 8000, "Port to bind")
+	dir := flag.String("dir", ".", "Directory to serve")
 
 	// Parse all flags
 	flag.Parse()
 
-	fmt.Printf("Serving files in the current directory on port %d\n", *portPtr)
-	http.Handle("/", &middleware{})
+	fmt.Printf("Serving files in the directory %s on port %d\n", *dir, *portPtr)
+	http.Handle("/", &middleware{dir: *dir})
 	err := http.ListenAndServe(fmt.Sprintf(":%d", *portPtr), nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
